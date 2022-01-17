d2f5f22c5c0d7b9b7fd27a6e68320058a1315330
João Vitor S. Oliveira <52728166+joaovitor-oliveira@users.noreply.github.com>
2022-01-17 15:26:57 -0300

    Adicionando a solução para o problema do troco

---
+++ troco.py
@@ -0,0 +1,17 @@
+def troco(l, target):
+    strike = [False] * (target + 1)
+    strike[0] = True
+
+    for moeda in l:
+        valor = target
+        while valor >= moeda:
+            if strike[valor - moeda]:
+                strike[valor] = True
+            valor -= 1
+
+    return strike[target]
+
+
+if __name__ == '__main__':
+    print(troco([25, 10, 5, 1], 16))
+    print(troco([33, 33, 32, 31, 30, 25, 20, 14, 7, 1], 33))
